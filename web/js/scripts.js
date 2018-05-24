3a2e6d8b1895aa3353a3e72a10938737561f3821
K-Flo <krystianflores@gmail.com>
2018-05-23 22:42:40 -0700

    got example loaded but not https :(

---
+++ web/js/scripts.js
@@ -0,0 +1,17 @@
+function init(){
+    var colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
+  
+    colors.on('track', function(event) {
+      if (event.data.length === 0) {
+        // No colors were detected in this frame.
+      } else {
+        event.data.forEach(function(rect) {
+          console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+        });
+      }
+    });
+  
+    tracking.track('#myVideo', colors, {camera: true});
+    // tracking.track('#video', tracker, { camera: true });
+  }
+  window.onload = init();
